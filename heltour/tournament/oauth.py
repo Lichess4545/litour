46284219c2c6b8a28b2c0c1aaebe9fe3474a41d1
Lakin Wecker <lakin@gratch.taildb4d09.ts.net>
2025-07-12 14:53:28 -0600

    chore(tests): fixing the envs and some tests

--- heltour/tournament/oauth.py
+++ heltour/tournament/oauth.py
@@ -163,7 +163,7 @@ def _code_challenge(verifier):
 
 
 def _get_redirect_uri(request):
-    http_url =  request.build_absolute_uri(reverse('lichess_auth'))
+    http_url = request.build_absolute_uri(reverse('lichess_auth'))
     return http_url.replace('http://', settings.LICHESS_OAUTH_REDIRECT_SCHEME)
 
 
