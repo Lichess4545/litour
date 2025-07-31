8e0bb5fcefded335cc1dab5b446cb64f803f8d10
glbert-does <tachydromos@hotmail.com>
2025-07-31 17:32:18 +0200

    add start_games action to round

--- heltour/tournament/admin.py
+++ heltour/tournament/admin.py
@@ -1413,6 +1413,7 @@ class RoundAdmin(_BaseAdmin):
         "simulate_results",
         "create_broadcast_round",
         "update_broadcast_round",
+        "start_games",
         "start_clocks",
     ]
     league_id_field = 'season__league_id'
@@ -1490,10 +1491,27 @@ class RoundAdmin(_BaseAdmin):
         self.message_user(request, 'Simulation complete.', messages.INFO)
         return redirect('admin:tournament_round_changelist')
 
+    def start_games(self, request, queryset):
+        if queryset.count() != 1:
+            self.message_user(request, "Can only start games one round at a time", messages.ERROR)
+            return
+        round_ = queryset[0]
+        if round_.is_scheduling_league():
+            self.message_user(
+                request,
+                "Attempting to start games for a scheduling league. Change league setting first.",
+                messages.ERROR,
+            )
+            return
+        self.message_user(request, "Starting games.", messages.INFO)
+        signals.do_start_unscheduled_games.send(sender=request.user, round_id=round_.pk)
+
     def start_clocks(self, request, queryset):
         if queryset.count() != 1:
             self.message_user(
-                request, "Starting the clock for more than one round at a time does not make sense."
+                request,
+                "Starting the clock for more than one round at a time does not make sense.",
+                messages.ERROR,
             )
             return
         round_ = queryset[0]
