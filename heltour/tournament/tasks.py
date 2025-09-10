a5e56bef171b28c7247e993eee04e17383b5a5a1
glbert-does <tachydromos@hotmail.com>
2025-09-10 15:28:22 +0200

    add back in single line that went missing from commit 40d62f6

--- heltour/tournament/tasks.py
+++ heltour/tournament/tasks.py
@@ -601,7 +601,7 @@ def _init_start_league_games(
     round_ = Round.objects.filter(
         season__league=league, is_completed=False, publish_pairings=True
     ).first()
-    signals.do_update_broadcast_round.send(sender="start_games", round_=round_)
+    signals.do_update_broadcast_round.send(sender="start_games", round_id=round_.pk)
     return result
 
 
