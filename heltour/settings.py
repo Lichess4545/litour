566c350549dc22827cfec9cae3d182240a04180b
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-09-13 08:50:02 -0400

    refactor: update database configuration to use secrets and enhance environment variable handling

--- heltour/settings.py
+++ heltour/settings.py
@@ -11,7 +11,7 @@ import environ
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Initialize environ
-env = environ.Env(
+env = environ.FileAwareEnv(
     # Set casting, default values
     DEBUG=(bool, False),
     STATIC_ROOT=(str, ""),
@@ -35,30 +35,17 @@ env = environ.Env(
     EMAIL_HOST=(str, ""),
     EMAIL_HOST_USER=(str, ""),
     EMAIL_HOST_PASSWORD=(str, ""),
-    EMAIL_HOST_USER_FILE=(str, ""),
-    EMAIL_HOST_PASSWORD_FILE=(str, ""),
     CELERY_DEFAULT_QUEUE=(str, "heltour-{}"),
     REDIS_URL=(str, "redis://localhost:6379"),
     SLEEP_UNIT=(float, 1.0),
     SECRET_KEY=(str, "this-is-only-for-testing"),
-    SECRET_KEY_FILE=(str, ""),
 )
 
 # Read .env file if it exists
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-
-
-def get_env_var_from_file_or_env(var_name, default=""):
-    """Helper function to read an environment variable from a file or directly."""
-    file_var_name = f"{var_name}_FILE"
-    if env(file_var_name, "") and os.path.exists(env(file_var_name)):
-        with open(env(file_var_name), "r") as f:
-            return f.read().strip()
-    return env(var_name, default=default)
-
+environ.FileAwareEnv.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_var_from_file_or_env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
@@ -214,8 +201,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="localhost")
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_USE_TLS = env("EMAIL_USE_TLS")
-EMAIL_HOST_USER = get_env_var_from_file_or_env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = get_env_var_from_file_or_env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 SERVER_EMAIL = env("SERVER_EMAIL", default="webmaster@lots.lichess.ca")
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="webmaster@lots.lichess.ca")
