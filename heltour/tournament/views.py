dc4e8a98d89c61e4f7e93961aea51d208a4473e1
Lakin Wecker <lakin.wecker@gmail.com>
2026-02-28 11:52:51 -0700

    fix: import signals (#107)

--- heltour/tournament/views.py
+++ heltour/tournament/views.py
@@ -25,7 +25,7 @@ from django.utils.text import slugify
 from django.views.generic import View
 from icalendar import Calendar, Event
 
-from heltour.tournament import alternates_manager, lichessapi, oauth, uptime
+from heltour.tournament import alternates_manager, lichessapi, oauth, signals, uptime
 from heltour.tournament.forms import (
     ContactForm,
     DeleteNominationForm,
